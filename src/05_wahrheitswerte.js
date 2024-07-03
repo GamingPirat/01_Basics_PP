a37ea08f2356ae2abd5c5b957f792d82067f99ee
Michael <161446509+GamingPirat@users.noreply.github.com>
2024-07-03 07:22:49 +0000

    05 / 02 Iteration 2 und 3

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,12 +6,27 @@ let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2024;
+// // Berechnung Alter Iteration 1
+// let year = 2024;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration 2
+// let date = new Date();
+// let year = date.getFullYear();
+// console.log("Datum " + date);
+// console.log("Jahr " + date.getFullYear());
+// console.log("Jahr " + year);
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration 3
+let year = new Date().getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
-// Ausgabe
+
+// // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
