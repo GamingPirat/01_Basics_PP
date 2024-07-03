a9169802271f2d5bbc7db45af072cb77b53eb443
Michael <161446509+GamingPirat@users.noreply.github.com>
2024-07-03 13:21:26 +0000

    07 / 01 codechallenge_II / Hausaufgabe

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -18,4 +18,32 @@ Bitte ggf. ein Struktogramm erstellen in Struktog
 
 /* Getränke-Challenge */
 
+const prompt = require('prompt-sync')({sigint: true});
 
+const name = prompt("Bitte geben Sie Ihren Namen ein:");
+
+let age;
+while (true) {
+  age = prompt("Bitte geben Sie Ihr Alter ein:");
+  if (!isNaN(age) && age >= 0) {
+    age = parseInt(age);
+    break;
+  } else {
+    console.log("Bitte geben Sie nur Zahlen ein!");
+  }
+}
+console.log(`Name: ${name}`);
+console.log(`Alter: ${age}`);
+
+let drink;
+if (age > 0 && age < 6) {
+  drink = "Milch";
+} else if (age >= 6 && age <= 12) {
+  drink = "Saft";
+} else if (age >= 13 && age <= 17) {
+  drink = "Cola";
+} else {
+  drink = "Wein";
+}
+
+console.log(`${name} trinkt ${drink}.`);
