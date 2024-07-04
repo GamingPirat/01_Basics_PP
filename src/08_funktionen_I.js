20572485226c49824c7a0de597ecff2ad158faf3
Michael <161446509+GamingPirat@users.noreply.github.com>
2024-07-04 08:20:23 +0000

    08 / 01 funktionen 01

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,15 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+
+// Funktionsaufruf | call
+// test();
+
+
+// Funtionsrumpf | callee
+// Funktionsdeklaration
+function test()
+{
+console.log("Hallo, Marius!");
+}
