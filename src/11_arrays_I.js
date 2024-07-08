6bc19a499bf305d058369ea3aeb146b3247008a5
Michael <161446509+GamingPirat@users.noreply.github.com>
2024-07-08 09:08:57 +0000

    11 / 3 Inkrement (var. Schrittweite)

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -19,6 +19,44 @@ let arr;
 // output(arr[0]);  // Index 0 (1. Pos. im Array)
 // output(arr[arr.length-1]);  //letzte postMessage. / allgemein
 
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
+
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i <= 5; i++) {   // i --> Schleifen-Index
+    // output(i)
+}
+
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i >= 0; i--) {   // i --> Schleifen-Index
+    // output(i)
+}
+
+// Inkrement (var. Schrittweite)
+for (let i = 10; i <= 100; i += 5) 
+{
+   output(i);   
+}
+
 
 /**
  *   PRAXIS
@@ -45,25 +83,8 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
     return str;
 }
 
-/*** 01a. Funktionalität mit Array 1 */
-// Kritik | Überlegung
-// word1,word2, .... wordN :: Semantische Struktur
-// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
-// --> Transformation semantisches Problem --> numerisches Problem
-output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-function getSentenceArr(arr) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   arr[0] + GAP +
-                arr[1] + GAP +
-                arr[2] + GAP +
-                arr[3] + GAP +
-                arr[4] + GAP +
-                arr[5] +
-                PUNCT;
 
-    return str;
-}
+
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
