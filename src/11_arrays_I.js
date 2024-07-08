65b3f6878938892ef38e447fb8766eca835d67c5
Michael <161446509+GamingPirat@users.noreply.github.com>
2024-07-08 10:02:04 +0000

    11 / 4 Transonierung

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -8,7 +8,7 @@
 
 /*** 02. Theorie: Array */
 
-let arr;
+// let arr;
 // arr = new Array();  // Konstruktor
 // arr = []
 // arr = [2,11,7,8,10];
@@ -25,44 +25,102 @@ let arr;
 // arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
 // --> Transformation semantisches Problem --> numerisches Problem
 // output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
-function getSentenceArr(arr) {
-    const GAP = " ";
-    const PUNCT = ".";
-    let str =   arr[0] + GAP +
-                arr[1] + GAP +
-                arr[2] + GAP +
-                arr[3] + GAP +
-                arr[4] + GAP +
-                arr[5] +
-                PUNCT;
+// function getSentenceArr(arr) {
+    // const GAP = " ";
+    // const PUNCT = ".";
+    // let str =   arr[0] + GAP +
+    //             arr[1] + GAP +
+    //             arr[2] + GAP +
+    //             arr[3] + GAP +
+    //             arr[4] + GAP +
+    //             arr[5] +
+    //             PUNCT;
 
-    return str;
-}
+//     return str;
+// }
 
 /* 02a. Theorie: Schleifen (for-schleife) */
 /* For -Schleifen als allg. Wiederholungs-Struktur */
 // Inkrement (untere Grenze --> obere Grenze)
 // for (let i = 0; i <= 5; i++) {   // i --> Schleifen-Index
     // output(i)
-}
+// }
 
 // Dekrement (obere Grenze --> untere Grenze)
 // for (let i = 10; i >= 0; i--) {   // i --> Schleifen-Index
     // output(i)
-}
+// }
 
 // Inkrement (var. Schrittweite)
-for (let i = 10; i <= 100; i += 5) 
-{
-   output(i);   
+// 
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) 
+// {
+//    output(i);    // interne Index
+//    output(arr[i]);  // Mapping intern --> extern
+// }
+
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1; // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//     // a = a + 1
+//     a += 1; // a = a + 1
+//     output("inhalt von a: " + a);
+// }
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+let str = ""; // Anfangswert
+for (let i = 0; i < 10; i++) {
+   str += "Test" + " "; // a = a + 1
+   output(str);
 }
+
+
 
 
 /**
  *   PRAXIS
  * 
  */
-
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    for (let i = 0; i < arr.length; i++) 
+{
+   output(arr[i]);  // Mapping intern --> extern
+}
+    return str;
+}
 
 
 /*** 01. Funktionalität mit Einzelparametern */
